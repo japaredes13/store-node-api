eb4d73151fdad5599f534ddf66982504cc478f98
Javier <jparedes@roshka.com>
2022-11-19 00:58:17 -0300

    ORM con la librería sequelize

--- services/productService.js
+++ services/productService.js
@@ -1,14 +1,14 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgresPool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
+    //this.pool = pool;
+    //this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -35,8 +35,8 @@ class ProductsService {
 
   async getAll() {
     const query = 'SELECT * FROM tasks';
-    const products = await this.pool.query(query);
-    return products.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
