0abc47652df780479ebe9dd5960178aa5a19e0ac
Javier <jparedes@roshka.com>
2022-11-21 23:30:51 -0300

    Estructura nueva para Customer y relacion BD con la tabla users

---
+++ routes/customersRouter.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const customerService = require('../services/customerService');
+const validatorHandler = require('../middlewares/validator.handler');
+const { updateCustomerSchema, createCustomerSchema, getCustomerSchema } = require('../schemas/customerSchema');
+
+const router = express.Router();
+const service = new customerService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id',
+  validatorHandler(getCustomerSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createCustomerSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getCustomerSchema, 'params'),
+  validatorHandler(updateCustomerSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getCustomerSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
+
---
+++ services/customerService.js
@@ -0,0 +1,40 @@
+const boom = require('@hapi/boom');
+
+const { models }= require('../libs/sequelize');
+
+class CustomerService {
+  constructor() {
+
+  }
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async find() {
+    const customers = await models.Customer.findAll();
+    return customers;
+  }
+
+  async findOne(id) {
+    const customer = await models.Customer.findByPk(id);
+    if (!customer) throw boom.notFound("Cliente no encontrado");
+
+    return customer;
+  }
+
+  async update(id, data) {
+    const customer = await this.findOne(id);
+    const updateCustomer = await customer.update(data);
+    return updateCustomer;
+  }
+
+  async delete(id) {
+    const customer = await this.findOne(id);
+    await customer.destroy();
+    return { id };
+  }
+}
+
+module.exports = CustomerService;
