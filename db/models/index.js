87224a04e3ac6f5a7f4e903ef32e60c10797ceb5
Javier <jparedes@roshka.com>
2023-04-22 12:59:28 -0400

    Estructura de órdenes de compra con cliente

--- db/models/index.js
+++ db/models/index.js
@@ -2,17 +2,20 @@ const { User, UserSchema } = require('./userModel');
 const { Customer, CustomerSchema} = require('./customerModel');
 const { Category, CategorySchema } = require('./categoryModel');
 const { Product, ProductSchema} = require('./productModel');
+const { Order, OrderSchema } = require('./orderModel');
 
 function setUpModels(sequelize){
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  Order.init(OrderSchema, Order.config(sequelize));
 
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
   Category.associate(sequelize.models);
   Product.associate(sequelize.models);
+  Order.associate(sequelize.models);
 }
 
 module.exports = setUpModels;
--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const OrderService = require('./../services/orderService');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { getOrderSchema, createOrderSchema } = require('../schemas/orderSchema');
 
 const router = express.Router();
+const service = new OrderService();
 
-router.get('/', (req, res) => {
-  res.json([]);
-});
+router.get('/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const {id} = req.params;
+      const order = await service.findOne(id);
+
+      res.json(order);
+    } catch (error) {
+      next(error)
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createOrderSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newProduct = await service.create(body);
+      res.status(201).json(
+        {
+          message: 'created',
+          data : newProduct
+        }
+      );
+    } catch (error) {
+      next(error)
+    }
+
+  }
+);
 
 module.exports = router;
